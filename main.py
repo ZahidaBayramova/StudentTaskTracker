67e41a50921edb8c8497cb348aa6b7e7cd0d4b56
ZahidaBayramova <zahidebayramova64@gmail.com>
2026-03-08 16:40:05 +0400

    SQL Database

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import oracledb
 conn=oracledb.connect(user="SYSTEM",password="Zahide54321@",dsn="192.168.0.196/XEPDB1")
 cur=conn.cursor()
-cur.execute("SELECT student_id from students")
+cur.execute("SELECT title from tasks")
 rows = cur.fetchall()
 if rows:
-    for  student_id in rows:
-        print(f" Student ID: {student_id}")
+    for  title in rows:
+        print(f" TASK ID: {title}")
 else:
-    print("Cədvəldə student yooxdur")
+    print("Cədvəldə title  yooxdur")
 
 cur.close()
 conn.close()
