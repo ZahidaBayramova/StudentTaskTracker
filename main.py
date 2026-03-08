0c117e36e2ba3f9f8e953eaffb03ad71b02d66af
ZahidaBayramova <zahidebayramova64@gmail.com>
2026-03-08 20:55:27 +0400

    Add .env and read data

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
+import os
+from dotenv import load_dotenv
 import oracledb
-conn=oracledb.connect(user="SYSTEM",password="Zahide54321@",dsn="192.168.0.196/XEPDB1")
-cur=conn.cursor()
-cur.execute("SELECT title from tasks")
-rows = cur.fetchall()
-if rows:
-    for  title in rows:
-        print(f" TASK ID: {title}")
-else:
-    print("Cədvəldə title  yooxdur")
-
-cur.close()
-conn.close()
+load_dotenv()
+user=os.getenv("DB_USER")
+password=os.getenv("DB_PASSWORD")
+dsn=os.getenv("DB_DSN")
+connection=oracledb.connect(user=user,password=password,dsn=dsn)
+for i in range(1,21):
+    sql="SELECT *FROM TASKS WHERE STUDENT_ID=:1"
+    odf=connection.fetch_df_all(sql,parameters=[i],arraysize=100)
+    print(odf.column_names())
+    print(odf.num_columns())
+    print(odf.num_rows())
