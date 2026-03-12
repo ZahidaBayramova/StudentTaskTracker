ba7cb2929740c7baf21c095433c9c4df2bef40c6
ZahidaBayramova <zahidebayramova64@gmail.com>
2026-03-12 22:59:23 +0400

    task adding

--- main.py
+++ main.py
@@ -6,9 +6,42 @@ user=os.getenv("DB_USER")
 password=os.getenv("DB_PASSWORD")
 dsn=os.getenv("DB_DSN")
 connection=oracledb.connect(user=user,password=password,dsn=dsn)
-for i in range(1,21):
-    sql="SELECT *FROM TASKS WHERE STUDENT_ID=:1"
-    odf=connection.fetch_df_all(sql,parameters=[i],arraysize=100)
-    print(odf.column_names())
-    print(odf.num_columns())
-    print(odf.num_rows())
+
+move = int(input("Choose an option (1-5): "))
+def choose(move):
+    match move:
+        case 1:
+            add_task()
+        case 2:
+            return "View all tasks"
+        case 3:
+            return "Update task status"
+        case 4:
+            return "Delete task"
+        case 5:
+            return "Exit"
+num_of_tasks=int(input"Enter the number of task you want add:")
+
+def add_task():
+    title=input("Please enter task title:")
+    description=input("Enter description for task")
+
+    student_id=int(input("Enter student ID:"))
+    teacher_id=int(input("Enter teacher ID:"))
+    cursor=connection.cursor()
+    insert_query="""
+INSERT INTO TASKS(title,description,student_id,teacher_id)
+VALUES(:title,:description,:student_id,:teacher_id)
+"""
+    cursor.execute(insert_query,{
+        "title":title,
+        "description":description,
+        "student_id":student_id,
+        "teacher_id":teacher_id
+    })
+    connection.commit()
+    print("Task added!")
+for _ in range(num_of_tasks):
+    add_task()
+    
+
